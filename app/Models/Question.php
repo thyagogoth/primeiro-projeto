40b175b7577dff4d17bd96ece05c89c9061a1d96
Thiago F. da Rosa <thyagogoth@gmail.com>
2024-11-16 18:10:46 -0300

    PP-6: creating seeder and factory for question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Database\Factories\QuestionFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    /** @use HasFactory<QuestionFactory> */
+    use HasFactory;
     //    protected $fillable = ['question'];
 
     protected $guarded = [];
