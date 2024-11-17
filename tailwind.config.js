703a7ac0f9d144e106ab51950b48be15d78f5c6c
Thiago F. da Rosa <thyagogoth@gmail.com>
2024-11-17 10:18:43 -0300

    PP-6: creating frontend of our form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import flowbite from 'flowbite/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,6 +8,7 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./node_modules/flowbite/**/*.js"
     ],
 
     theme: {
@@ -16,6 +18,10 @@ export default {
             },
         },
     },
+    darkMode: 'class',
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        flowbite
+    ],
 };
