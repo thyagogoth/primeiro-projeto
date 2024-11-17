5093a1ba47cf5a36d9469deaf4702cc0084644ef
Thiago F. da Rosa <thyagogoth@gmail.com>
2024-11-17 11:47:57 -0300

    PP-6: refactor to use blade components

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,26 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <x-header>
             {{ __('Dashboard') }}
-        </h2>
+        </x-header>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <x-container>
+        <x-form post :action="route('question.store')">
+            <x-textarea label="Qual é a sua pergunta?" name="question" />
 
-            <form method="post" action="{{ route('question.store') }}">
-                @csrf
-                <div class="mb-4">
-                    <label for="question" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Qual é a sua pergunta?</label>
-                    <textarea id="question" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                          placeholder="Ask me anything..." name="question">{{ old('question') }}</textarea>
-                    @error('question') <span class="text-red-500">{{ $message }}</span> @enderror
-                </div>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                    ENVIAR
-                </button>
-            </form>
-
-        </div>
-    </div>
+            <x-button.primary>Save</x-button.primary>
+            <x-button.reset>Cancel</x-button.reset>
+        </x-form>
+    </x-container>
 </x-app-layout>
